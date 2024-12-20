b0bf3e50d7042a5e052dc61ec0185478b8c721b1
Quentin Lurkin <qlurkin@gmail.com>
2024-12-20 18:15:30 +0100

    Add RenderPass

--- gpu.py
+++ gpu.py
@@ -6,6 +6,40 @@ TEXTURE_FORMAT = TextureFormat.rgba8unorm
 ADAPTER = wgpu.gpu.request_adapter_sync(power_preference="high-performance")  # type: ignore
 DEVICE = ADAPTER.request_device_sync()
 
+VERTEX_FORMAT_SIZE = {
+    VertexFormat.float16x2: 4,
+    VertexFormat.float16x4: 8,
+    VertexFormat.float32: 4,
+    VertexFormat.float32x2: 8,
+    VertexFormat.float32x3: 12,
+    VertexFormat.float32x4: 16,
+    VertexFormat.sint16x2: 4,
+    VertexFormat.sint16x4: 8,
+    VertexFormat.sint32: 4,
+    VertexFormat.sint32x2: 8,
+    VertexFormat.sint32x3: 12,
+    VertexFormat.sint32x4: 16,
+    VertexFormat.sint8x2: 2,
+    VertexFormat.sint8x4: 4,
+    VertexFormat.snorm16x2: 4,
+    VertexFormat.snorm16x4: 8,
+    VertexFormat.snorm8x2: 2,
+    VertexFormat.snorm8x4: 4,
+    VertexFormat.uint16x2: 4,
+    VertexFormat.uint16x4: 8,
+    VertexFormat.uint32: 4,
+    VertexFormat.uint32x2: 8,
+    VertexFormat.uint32x3: 12,
+    VertexFormat.uint32x4: 16,
+    VertexFormat.uint8x2: 2,
+    VertexFormat.uint8x4: 4,
+    VertexFormat.unorm10_10_10_2: 4,
+    VertexFormat.unorm16x2: 4,
+    VertexFormat.unorm16x4: 8,
+    VertexFormat.unorm8x2: 2,
+    VertexFormat.unorm8x4: 4,
+}
+
 
 class Texture:
     def __init__(self, size: tuple[int, int]):
@@ -44,38 +78,77 @@ class GraphicPipelineBuilder:
     def __init__(self):
         self.shader_module = None
         self.buffers = []
+        self.location = 0
 
-    def shader(self, filename):
+    def with_shader(self, filename):
         with open(filename) as file:
             shader_source = file.read()
         self.shader_module = DEVICE.create_shader_module(code=shader_source)
         return self
 
+    def with_buffer(
+        self, step_mode: VertexStepMode | str, array_stride: int | None = None
+    ):
+        self.buffers.append(
+            {
+                "array_stride": array_stride,
+                "step_mode": step_mode,
+                "attributes": [],
+            }
+        )
+        return self
+
+    def with_vertex_buffer(self, array_stride: int | None = None):
+        self.with_buffer(VertexStepMode.vertex, array_stride)
+        return self
+
+    def with_instance_buffer(self, array_stride: int | None = None):
+        self.with_buffer(VertexStepMode.instance, array_stride)
+        return self
+
+    def with_attribute(
+        self,
+        format: VertexFormat | str,
+        offset: int | None = None,
+        location: int | None = None,
+    ):
+        if location is None:
+            location = self.location
+        self.location = location + 1
+
+        if offset is None:
+            if len(self.buffers[-1]["attributes"]) == 0:
+                offset = 0
+            else:
+                prev = self.buffers[-1]["attributes"][-1]
+                offset = prev["offset"] + VERTEX_FORMAT_SIZE[prev["format"]]
+
+        self.buffers[-1]["attributes"].append(
+            {
+                "format": format,
+                "offset": offset,
+                "shader_location": location,
+            }
+        )
+        return self
+
     def build(self):
         # pipeline_layout = DEVICE.create_pipeline_layout(bind_group_layouts=[])
+
+        for buffer in self.buffers:
+            if buffer["array_stride"] is None:
+                last_attribute = buffer["attributes"][-1]
+                buffer["array_stride"] = (
+                    last_attribute["offset"]
+                    + VERTEX_FORMAT_SIZE[last_attribute["format"]]
+                )
+
         params = {
             "layout": "auto",
             "vertex": {
                 "module": self.shader_module,
                 "entry_point": "vs_main",
-                "buffers": [
-                    {
-                        "array_stride": 4 * 5,
-                        "step_mode": VertexStepMode.vertex,
-                        "attributes": [
-                            {
-                                "format": VertexFormat.float32x2,
-                                "offset": 0,
-                                "shader_location": 0,
-                            },
-                            {
-                                "format": VertexFormat.float32x3,
-                                "offset": 4 * 2,
-                                "shader_location": 1,
-                            },
-                        ],
-                    },
-                ],
+                "buffers": self.buffers,
             },
             "depth_stencil": None,
             "multisample": None,
@@ -99,6 +172,7 @@ class GraphicPipelineBuilder:
 class GraphicPipeline:
     def __init__(self, params: dict):
         self.render_pipeline = DEVICE.create_render_pipeline(**params)
+        # get_bind_group_layout(index: int)
         self.vertex_buffer = None
 
     def set_vertex_buffer(self, buffer: Buffer):
@@ -124,3 +198,36 @@ class GraphicPipeline:
         render_pass.draw(3, 1, 0, 0)
         render_pass.end()
         DEVICE.queue.submit([command_encoder.finish()])
+
+
+class RenderPass:
+    def __init__(self, texture: Texture):
+        self.vertex_buffer_index = 0
+        self.command_encoder = DEVICE.create_command_encoder()
+        self.render_pass = self.command_encoder.begin_render_pass(
+            color_attachments=[
+                {
+                    "view": texture.view,
+                    "resolve_target": None,
+                    "clear_value": (0, 0, 0, 1),
+                    "load_op": wgpu.LoadOp.clear,
+                    "store_op": wgpu.StoreOp.store,
+                }
+            ]
+        )
+
+    def with_pipeline(self, pipeline: GraphicPipeline):
+        self.render_pass.set_pipeline(pipeline.render_pipeline)
+        return self
+
+    def with_vertex_buffer(self, buffer: Buffer):
+        self.render_pass.set_vertex_buffer(self.vertex_buffer_index, buffer.data)
+        return self
+
+    def draw(self, vertex_count, instance_count=1):
+        self.render_pass.draw(vertex_count, instance_count, 0, 0)
+        return self
+
+    def submit(self):
+        self.render_pass.end()
+        DEVICE.queue.submit([self.command_encoder.finish()])
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from gpu import Texture, GraphicPipelineBuilder, Buffer
+from wgpu import VertexFormat
+from gpu import Texture, GraphicPipelineBuilder, Buffer, RenderPass
 from window import Window, App, Surface, surface_from_texture
 import numpy as np
 
@@ -15,14 +16,24 @@ class MyApp(App):
             dtype=np.float32,
         )
 
-        vertex_buffer = Buffer(vertex_data)
+        self.vertex_buffer = Buffer(vertex_data)
 
         self.texture = Texture(size)
-        self.pipeline = GraphicPipelineBuilder().shader("shader.wgsl").build()
-        self.pipeline.set_vertex_buffer(vertex_buffer)
+        self.pipeline = (
+            GraphicPipelineBuilder()
+            .with_shader("shader.wgsl")
+            .with_vertex_buffer()
+            .with_attribute(VertexFormat.float32x2)
+            .with_attribute(VertexFormat.float32x3)
+            .build()
+        )
+        # self.pipeline.set_vertex_buffer(vertex_buffer)
 
     def render(self, screen: Surface):
-        self.pipeline.render(self.texture)
+        # self.pipeline.render(self.texture)
+        RenderPass(self.texture).with_pipeline(self.pipeline).with_vertex_buffer(
+            self.vertex_buffer
+        ).draw(3).submit()
         surface = surface_from_texture(self.texture)
         screen.blit(surface)
 
