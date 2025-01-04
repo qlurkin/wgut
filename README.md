04562fbaa5175e38b03eb22a79f7ae40dff37882
Quentin Lurkin <qlurkin@gmail.com>
2025-01-04 20:53:29 +0100

    Move examples

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # WebGPU Utility Toolkit
 
 Play with `wgpu`
+
+## Examples
+
+You can use the `run_example.py` script to test the examples from the `examples/` folder.
+
+```
+python run_example.py triangle
+```
---
+++ run_example.py
@@ -0,0 +1,20 @@
+import sys
+import os
+
+if len(sys.argv) < 2:
+    raise Exception("Add the name of an example without the `.py`")
+
+name = sys.argv[1]
+name = name + ".py"
+
+examples = os.listdir("./examples")
+
+if name not in examples:
+    raise Exception("Not an example")
+
+os.chdir("./examples")
+
+with open(name) as file:
+    source = file.read()
+
+exec(source)
