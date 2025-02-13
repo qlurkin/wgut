7640a9bd2aeca06d6734bb9fd4724cc7890aae76
Quentin Lurkin <qlurkin@gmail.com>
2025-02-13 12:56:50 +0100

    Add Auto Render Pipeline

---
+++ examples/cube_with_auto_pipeline.py
@@ -0,0 +1,109 @@
+from wgpu import BufferUsage, GPUTexture, IndexFormat, ShaderStage, VertexFormat
+from wgut.builders import (
+    BindGroupBuilder,
+    BingGroupLayoutBuilder,
+    RenderPipelineBuilder,
+    VertexBufferDescriptorsBuilder,
+    BufferBuilder,
+    CommandBufferBuilder,
+    PipelineLayoutBuilder,
+    TextureBuilder,
+    write_buffer,
+)
+from wgut.auto_render_pipeline import AutoRenderPipeline
+from wgut.window import Window
+import numpy as np
+import wgut.cgmath as cm
+
+
+class MyApp(Window):
+    def on_resize(self):
+        canvas = self.get_canvas()
+        self.pipeline.create_depth_texture(canvas.get_physical_size())
+
+        width, height = canvas.get_logical_size()
+
+        view_matrix = cm.look_at([3, 2, 4], [0, 0, 0], [0, 1, 0])
+        proj_matrix = cm.perspective(45, width / height, 0.1, 100)
+
+        camera_data = np.array([view_matrix, proj_matrix])
+
+        self.pipeline.set_array(0, 0, camera_data)
+
+    def setup(self):
+        self.set_title("Hello Cube")
+
+        # fmt: off
+        vertex_data = np.array(
+            [
+                # x, y, z, r, g, b
+                [0.5, 0.5, 0.5, 1.0, 0.0, 0.0],
+                [-0.5, 0.5, 0.5, 1.0, 0.0, 0.0],
+                [-0.5, -0.5, 0.5, 1.0, 0.0, 0.0],
+                [0.5, -0.5, 0.5, 1.0, 0.0, 0.0],
+
+                [0.5, 0.5, 0.5, 0.0, 1.0, 0.0],
+                [0.5, -0.5, 0.5, 0.0, 1.0, 0.0],
+                [0.5, -0.5, -0.5, 0.0, 1.0, 0.0],
+                [0.5, 0.5, -0.5, 0.0, 1.0, 0.0],
+
+                [0.5, 0.5, -0.5, 0.0, 0.0, 1.0],
+                [0.5, -0.5, -0.5, 0.0, 0.0, 1.0],
+                [-0.5, -0.5, -0.5, 0.0, 0.0, 1.0],
+                [-0.5, 0.5, -0.5, 0.0, 0.0, 1.0],
+
+                [-0.5, 0.5, 0.5, 1.0, 1.0, 0.0],
+                [-0.5, 0.5, -0.5, 1.0, 1.0, 0.0],
+                [-0.5, -0.5, -0.5, 1.0, 1.0, 0.0],
+                [-0.5, -0.5, 0.5, 1.0, 1.0, 0.0],
+
+                [0.5, 0.5, 0.5, 0.0, 1.0, 1.0],
+                [0.5, 0.5, -0.5, 0.0, 1.0, 1.0],
+                [-0.5, 0.5, -0.5, 0.0, 1.0, 1.0],
+                [-0.5, 0.5, 0.5, 0.0, 1.0, 1.0],
+    
+                [0.5, -0.5, 0.5, 1.0, 0.0, 1.0],
+                [-0.5, -0.5, 0.5, 1.0, 0.0, 1.0],
+                [-0.5, -0.5, -0.5, 1.0, 0.0, 1.0],
+                [0.5, -0.5, -0.5, 1.0, 0.0, 1.0],
+            ],
+            dtype=np.float32,
+        )
+
+        index_data = np.array(
+            [
+                 0,  1,  2,  0,  2,  3,
+                 4,  5,  6,  4,  6,  7,
+                 8,  9, 10,  8, 10, 11,
+                12, 13, 14, 12, 14, 15,
+                16, 17, 18, 16, 18, 19,
+                20, 21, 22, 20, 22, 23,
+            ],
+            dtype=np.uint32,
+        )
+        # fmt: on
+
+        vertex_buffer_descriptors = (
+            VertexBufferDescriptorsBuilder()
+            .with_vertex_buffer()
+            .with_attribute(VertexFormat.float32x3)
+            .with_attribute(VertexFormat.float32x3)
+            .build()
+        )
+
+        self.pipeline = AutoRenderPipeline("cube.wgsl", vertex_buffer_descriptors)
+
+        self.pipeline.set_vertex_array(0, vertex_data)
+        self.pipeline.set_index_array(index_data)
+
+        self.on_resize()
+
+    def render(self, screen: GPUTexture):
+        self.pipeline.render(screen, 36)
+
+    def process_event(self, event):
+        if event["event_type"] == "resize":
+            self.on_resize()
+
+
+MyApp().run()
---
+++ src/wgut/auto_render_pipeline.py
@@ -0,0 +1,192 @@
+from wgut.reflection import Reflection
+from wgut.builders import (
+    BufferBuilder,
+    SamplerBuilder,
+    compile_slang,
+    BindGroupBuilder,
+    CommandBufferBuilder,
+    RenderPipelineBuilder,
+    TextureBuilder,
+)
+import numpy.typing as npt
+import numpy as np
+from wgpu import (
+    BufferUsage,
+    GPUBuffer,
+    GPUSampler,
+    GPUTexture,
+    IndexFormat,
+)
+
+
+class AutoRenderPipeline:
+    def __init__(
+        self,
+        shader_source_file: str,
+        vertex_buffer_descriptors: list,
+    ):
+        if shader_source_file.endswith(".slang"):
+            self.wgsl_source = compile_slang(shader_source_file)
+        else:
+            with open(shader_source_file) as file:
+                self.wgsl_source = file.read()
+
+        self.reflection = Reflection(self.wgsl_source)
+        self.bind_groups = {}
+        self.bindings = {}
+        for gid in self.reflection.get_bind_group_ids():
+            self.bind_groups[gid] = None
+            self.bindings[gid] = {}
+            for bid in self.reflection.get_binding_ids(gid):
+                self.bindings[gid][bid] = None
+        self.vertex_buffer_descriptors = vertex_buffer_descriptors
+        self.vertex_buffers: list[GPUBuffer | None] = [None] * len(
+            vertex_buffer_descriptors
+        )
+        self.index_buffer = None
+        self.index_format = IndexFormat.uint32
+        self.depth_texture = None
+        self.pipeline = None
+        self.output_format = None
+
+    def set_array(
+        self,
+        group: int,
+        binding: int,
+        array: npt.NDArray,
+        additional_usages: BufferUsage | int = 0,
+    ) -> GPUBuffer:
+        builder = BufferBuilder().from_data(array)
+        if self.reflection.get_binding_space(group, binding) == "uniform":
+            builder.with_usage(BufferUsage.UNIFORM | additional_usages)  # type: ignore
+        else:
+            builder.with_usage(BufferUsage.STORAGE | additional_usages)  # type: ignore
+
+        buffer = builder.build()
+        return self.set_buffer(group, binding, buffer)
+
+    def set_buffer(self, group: int, binding: int, buffer: GPUBuffer) -> GPUBuffer:
+        self.bind_groups[group] = None
+        self.bindings[group][binding] = buffer
+        return buffer
+
+    def set_texture(self, group: int, binding: int, texture: GPUTexture) -> GPUTexture:
+        self.bind_groups[group] = None
+        self.bindings[group][binding] = texture
+        return texture
+
+    def set_sampler(
+        self, group: int, binding: int, sampler: GPUSampler | None = None
+    ) -> GPUSampler:
+        if sampler is None:
+            sampler = SamplerBuilder().build()
+        self.bind_groups[group] = None
+        self.bindings[group][binding] = sampler
+        return sampler
+
+    def set_vertex_buffer(self, id: int, buffer: GPUBuffer) -> GPUBuffer:
+        self.vertex_buffers[id] = buffer
+        return buffer
+
+    def set_vertex_array(
+        self,
+        id: int,
+        array: npt.NDArray,
+        additional_usages: BufferUsage | int = 0,
+    ) -> GPUBuffer:
+        buffer = (
+            BufferBuilder()
+            .from_data(array)
+            .with_usage(BufferUsage.VERTEX | additional_usages)  # type: ignore
+            .build()
+        )
+        return self.set_vertex_buffer(id, buffer)
+
+    def set_index_buffer(
+        self, buffer: GPUBuffer, index_format: IndexFormat | str = IndexFormat.uint32
+    ) -> GPUBuffer:
+        self.index_buffer = buffer
+        self.index_format = index_format
+        return buffer
+
+    def set_index_array(
+        self, array: npt.NDArray, additional_usages: BufferUsage | int = 0
+    ) -> GPUBuffer:
+        if array.dtype == np.uint32:
+            index_format = IndexFormat.uint32
+        elif array.dtype == np.uint16:
+            index_format = IndexFormat.uint16
+        else:
+            raise TypeError(f"Invalid Index Array Type: {array.dtype}")
+
+        buffer = (
+            BufferBuilder()
+            .from_data(array)
+            .with_usage(BufferUsage.INDEX | additional_usages)  # type: ignore
+            .build()
+        )
+        return self.set_index_buffer(buffer, index_format)
+
+    def set_depth_texture(self, texture: GPUTexture) -> GPUTexture:
+        if self.depth_texture is None:
+            self.pipeline = None  # must recreate the pipeline
+        self.depth_texture = texture
+        return texture
+
+    def create_depth_texture(self, size: tuple[int, int]) -> GPUTexture:
+        texture = TextureBuilder().build_depth(size)
+        return self.set_depth_texture(texture)
+
+    def render(
+        self, output_texture: GPUTexture, vertex_count: int, instance_count: int = 1
+    ):
+        if self.output_format is None or self.output_format != output_texture.format:
+            self.output_format = output_texture.format
+            self.pipeline = None  # must recreate the pipeline
+
+        if self.pipeline is None:
+            pipeline_builder = (
+                RenderPipelineBuilder(
+                    self.output_format, self.vertex_buffer_descriptors
+                )
+                .with_layout(self.reflection.get_pipeline_layout())
+                .with_shader_source(self.wgsl_source)
+            )
+
+            if self.depth_texture is not None:
+                pipeline_builder.with_depth_stencil()
+
+            self.pipeline = pipeline_builder.build()
+
+        for gid in self.bind_groups:
+            if self.bind_groups[gid] is None:
+                layout = self.reflection.get_bind_group_layout(gid)
+                builder = BindGroupBuilder(layout)
+                for bid in self.bindings[gid]:
+                    if isinstance(self.bindings[gid][bid], GPUTexture):
+                        builder.with_texture(self.bindings[gid][bid], index=bid)
+                    elif isinstance(self.bindings[gid][bid], GPUSampler):
+                        builder.with_sampler(self.bindings[gid][bid], index=bid)
+                    else:
+                        builder.with_buffer(self.bindings[gid][bid], index=bid)
+                self.bind_groups[gid] = builder.build()
+        command_encoder = CommandBufferBuilder()
+        render_pass_builder = command_encoder.begin_render_pass(output_texture)
+        if self.depth_texture is not None:
+            render_pass_builder.with_depth_stencil(self.depth_texture)
+        render_pass = render_pass_builder.build()
+        render_pass.set_pipeline(self.pipeline)
+        for gid, g in self.bind_groups.items():
+            render_pass.set_bind_group(gid, g)
+        for id, buf in enumerate(self.vertex_buffers):
+            if buf is None:
+                raise ValueError(f"Vertex Buffer {id} not set")
+            render_pass.set_vertex_buffer(id, buf)
+        if self.index_buffer is None:
+            render_pass.draw(vertex_count, instance_count)
+        else:
+            render_pass.set_index_buffer(self.index_buffer, self.index_format)  # type: ignore
+            render_pass.draw_indexed(vertex_count, instance_count)
+        render_pass.end()
+
+        command_encoder.submit()
