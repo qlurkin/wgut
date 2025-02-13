f19491f2ee131fa61da5782749e7fd1530b3288f
Quentin Lurkin <qlurkin@gmail.com>
2025-02-13 11:11:31 +0100

    Add Sampler Builder

--- src/wgut/builders.py
+++ src/wgut/builders.py
@@ -567,6 +567,61 @@ class ComputePassBuilder(BuilderBase):
         )
 
 
+class SamplerBuilder(BuilderBase):
+    def __init__(self):
+        self.address_mode_u = wgpu.AddressMode.repeat
+        self.address_mode_v = wgpu.AddressMode.repeat
+        self.address_mode_w = wgpu.AddressMode.repeat
+        self.mag_filter = wgpu.FilterMode.nearest
+        self.min_filter = wgpu.FilterMode.nearest
+        self.mipmap_filter = wgpu.MipmapFilterMode.nearest
+
+    def with_address_mode_u(self, address_mode: wgpu.AddressMode | str) -> Self:
+        self.address_mode_u = address_mode
+        return self
+
+    def with_address_mode_v(self, address_mode: wgpu.AddressMode | str) -> Self:
+        self.address_mode_v = address_mode
+        return self
+
+    def with_address_mode_w(self, address_mode: wgpu.AddressMode | str) -> Self:
+        self.address_mode_w = address_mode
+        return self
+
+    def with_address_mode(self, address_mode: wgpu.AddressMode | str) -> Self:
+        self.address_mode_u = address_mode
+        self.address_mode_v = address_mode
+        self.address_mode_w = address_mode
+        return self
+
+    def with_mipmap_filter(self, mipmap_filter: wgpu.MipmapFilterMode | str) -> Self:
+        self.mipmap_filter = mipmap_filter
+        return self
+
+    def with_mag_filter(self, filter: wgpu.FilterMode | str) -> Self:
+        self.mag_filter = filter
+        return self
+
+    def with_min_filter(self, filter: wgpu.FilterMode | str) -> Self:
+        self.min_filter = filter
+        return self
+
+    def with_filter(self, filter: wgpu.FilterMode | str) -> Self:
+        self.mag_filter = filter
+        self.min_filter = filter
+        return self
+
+    def build(self) -> wgpu.GPUSampler:
+        return get_device().create_sampler(
+            address_mode_u=self.address_mode_u,  # type: ignore
+            address_mode_v=self.address_mode_v,  # type: ignore
+            address_mode_w=self.address_mode_w,  # type: ignore
+            mag_filter=self.mag_filter,  # type: ignore
+            min_filter=self.min_filter,  # type: ignore
+            mipmap_filter=self.mipmap_filter,  # type: ignore
+        )
+
+
 class BindGroupBuilder(BuilderBase):
     def __init__(self, layout):
         super().__init__()
@@ -609,20 +664,17 @@ class BindGroupBuilder(BuilderBase):
 
     def with_sampler(
         self,
-        address_mode: wgpu.AddressMode | str = wgpu.AddressMode.repeat,
-        filter: wgpu.FilterMode | str = wgpu.FilterMode.nearest,
+        sampler: wgpu.GPUSampler | None = None,
         index=None,
     ) -> Self:
         if index is None:
             index = self.index
+        if sampler is None:
+            sampler = SamplerBuilder().build()
         self.bindings.append(
             {
                 "binding": index,
-                "resource": get_device().create_sampler(
-                    address_mode_u=address_mode,  # type: ignore
-                    address_mode_v=address_mode,  # type: ignore
-                    mag_filter=filter,  # type: ignore
-                ),
+                "resource": sampler,
             }
         )
         self.index = index + 1
