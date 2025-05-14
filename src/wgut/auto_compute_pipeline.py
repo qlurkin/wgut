ff629dd303cfc9e536386ee28051c753a245341c
Quentin Lurkin <qlurkin@gmail.com>
2025-05-14 15:31:19 +0200

    More Encapsulation

--- src/wgut/auto_compute_pipeline.py
+++ src/wgut/auto_compute_pipeline.py
@@ -11,19 +11,19 @@ from wgpu import BufferUsage, GPUBuffer
 
 class AutoComputePipeline:
     def __init__(self, shader_source: str):
-        self.wgsl_source = shader_source
-        self.reflection = Reflection(self.wgsl_source)
-        self.bind_groups = {}
-        self.bindings = {}
-        for gid in self.reflection.get_bind_group_ids():
-            self.bind_groups[gid] = None
-            self.bindings[gid] = {}
-            for bid in self.reflection.get_binding_ids(gid):
-                self.bindings[gid][bid] = None
-        self.pipeline = (
+        self.__wgsl_source = shader_source
+        self.__reflection = Reflection(self.__wgsl_source)
+        self.__bind_groups = {}
+        self.__bindings = {}
+        for gid in self.__reflection.get_bind_group_ids():
+            self.__bind_groups[gid] = None
+            self.__bindings[gid] = {}
+            for bid in self.__reflection.get_binding_ids(gid):
+                self.__bindings[gid][bid] = None
+        self.__pipeline = (
             ComputePipelineBuilder()
-            .with_layout(self.reflection.get_pipeline_layout())
-            .with_shader_source(self.wgsl_source)
+            .with_layout(self.__reflection.get_pipeline_layout())
+            .with_shader_source(self.__wgsl_source)
             .build()
         )
 
@@ -35,7 +35,7 @@ class AutoComputePipeline:
         additional_usages: BufferUsage | int | str = 0,
     ) -> GPUBuffer:
         builder = BufferBuilder().from_data(array)
-        if self.reflection.get_binding_space(group, binding) == "uniform":
+        if self.__reflection.get_binding_space(group, binding) == "uniform":
             builder.with_usage(BufferUsage.UNIFORM | additional_usages)  # type: ignore
         else:
             builder.with_usage(BufferUsage.STORAGE | additional_usages)  # type: ignore
@@ -45,22 +45,22 @@ class AutoComputePipeline:
         return buffer
 
     def set_buffer(self, group: int, binding: int, buffer: GPUBuffer) -> GPUBuffer:
-        self.bind_groups[group] = None
-        self.bindings[group][binding] = buffer
+        self.__bind_groups[group] = None
+        self.__bindings[group][binding] = buffer
         return buffer
 
     def dispatch(self, x: int, y: int = 1, z: int = 1):
-        for gid in self.bind_groups:
-            if self.bind_groups[gid] is None:
-                layout = self.reflection.get_bind_group_layout(gid)
+        for gid in self.__bind_groups:
+            if self.__bind_groups[gid] is None:
+                layout = self.__reflection.get_bind_group_layout(gid)
                 builder = BindGroupBuilder(layout)
-                for bid in self.bindings[gid]:
-                    builder.with_buffer(self.bindings[gid][bid], index=bid)
-                self.bind_groups[gid] = builder.build()
+                for bid in self.__bindings[gid]:
+                    builder.with_buffer(self.__bindings[gid][bid], index=bid)
+                self.__bind_groups[gid] = builder.build()
         command_encoder = CommandBufferBuilder()
         compute_pass = command_encoder.begin_compute_pass().build()
-        compute_pass.set_pipeline(self.pipeline)
-        for gid, g in self.bind_groups.items():
+        compute_pass.set_pipeline(self.__pipeline)
+        for gid, g in self.__bind_groups.items():
             compute_pass.set_bind_group(gid, g)
         compute_pass.dispatch_workgroups(x, y, z)
         compute_pass.end()
--- src/wgut/auto_render_pipeline.py
+++ src/wgut/auto_render_pipeline.py
@@ -24,58 +24,58 @@ from typing import Self
 
 class AutoRenderPipeline:
     def __init__(self, shader_source: str):
-        self.wgsl_source = shader_source
+        self.__wgsl_source = shader_source
 
-        self.reflection = Reflection(self.wgsl_source)
-        self.bind_groups = {}
-        self.bindings = {}
-        for gid in self.reflection.get_bind_group_ids():
-            self.bind_groups[gid] = None
-            self.bindings[gid] = {}
-            for bid in self.reflection.get_binding_ids(gid):
-                self.bindings[gid][bid] = None
-        self.vertex_buffer_descriptors = []
-        self.vertex_buffers: list[GPUBuffer | None] = []
-        self.index_buffer = None
-        self.index_format = IndexFormat.uint32
-        self.depth_texture = None
-        self.with_depth = False
-        self.pipeline = None
-        self.output_format = None
-        self.vertex_descriptors_builder = None
+        self.__reflection = Reflection(self.__wgsl_source)
+        self.__bind_groups = {}
+        self.__bindings = {}
+        for gid in self.__reflection.get_bind_group_ids():
+            self.__bind_groups[gid] = None
+            self.__bindings[gid] = {}
+            for bid in self.__reflection.get_binding_ids(gid):
+                self.__bindings[gid][bid] = None
+        self.__vertex_buffer_descriptors = []
+        self.__vertex_buffers: list[GPUBuffer | None] = []
+        self.__index_buffer = None
+        self.__index_format = IndexFormat.uint32
+        self.__depth_texture = None
+        self.__with_depth = False
+        self.__pipeline = None
+        self.__output_format = None
+        self.__vertex_descriptors_builder = None
 
     def set_vertex_buffer_descriptors(self, vertex_buffer_descriptors: list) -> Self:
-        if self.vertex_descriptors_builder is not None:
+        if self.__vertex_descriptors_builder is not None:
             raise Exception(
                 "You already begin to create Vertex Buffer Descriptor with simple methods. You can't do both."
             )
-        self.vertex_buffer_descriptors = vertex_buffer_descriptors
-        self.vertex_buffers = [None] * len(
+        self.__vertex_buffer_descriptors = vertex_buffer_descriptors
+        self.__vertex_buffers = [None] * len(
             vertex_buffer_descriptors
         )  # Create slots for vertex buffers
-        self.pipeline = None  # must recreate the pipeline
+        self.__pipeline = None  # must recreate the pipeline
         return self
 
     def add_simple_vertex_descriptor(self, *vertex_formats: VertexFormat | str) -> Self:
-        if self.vertex_descriptors_builder is None:
-            self.vertex_descriptors_builder = VertexBufferDescriptorsBuilder()
-            self.vertex_buffers = []  # reset the buffers
-        self.vertex_descriptors_builder.with_vertex_buffer()
+        if self.__vertex_descriptors_builder is None:
+            self.__vertex_descriptors_builder = VertexBufferDescriptorsBuilder()
+            self.__vertex_buffers = []  # reset the buffers
+        self.__vertex_descriptors_builder.with_vertex_buffer()
         for format in vertex_formats:
-            self.vertex_descriptors_builder.with_attribute(format)
-        self.vertex_buffers.append(None)  # Add a slot for the vertex buffer
+            self.__vertex_descriptors_builder.with_attribute(format)
+        self.__vertex_buffers.append(None)  # Add a slot for the vertex buffer
         return self
 
     def add_simple_instance_descriptor(
         self, *vertex_formats: VertexFormat | str
     ) -> Self:
-        if self.vertex_descriptors_builder is None:
-            self.vertex_descriptors_builder = VertexBufferDescriptorsBuilder()
-            self.vertex_buffers = []  # reset the buffers
-        self.vertex_descriptors_builder.with_instance_buffer()
+        if self.__vertex_descriptors_builder is None:
+            self.__vertex_descriptors_builder = VertexBufferDescriptorsBuilder()
+            self.__vertex_buffers = []  # reset the buffers
+        self.__vertex_descriptors_builder.with_instance_buffer()
         for format in vertex_formats:
-            self.vertex_descriptors_builder.with_attribute(format)
-        self.vertex_buffers.append(None)  # Add a slot for the vertex buffer
+            self.__vertex_descriptors_builder.with_attribute(format)
+        self.__vertex_buffers.append(None)  # Add a slot for the vertex buffer
         return self
 
     def set_binding_array(
@@ -86,7 +86,7 @@ class AutoRenderPipeline:
         additional_usages: BufferUsage | int = 0,
     ) -> GPUBuffer:
         builder = BufferBuilder().from_data(array)
-        if self.reflection.get_binding_space(group, binding) == "uniform":
+        if self.__reflection.get_binding_space(group, binding) == "uniform":
             builder.with_usage(BufferUsage.UNIFORM | additional_usages)  # type: ignore
         else:
             builder.with_usage(BufferUsage.STORAGE | additional_usages)  # type: ignore
@@ -97,15 +97,15 @@ class AutoRenderPipeline:
     def set_binding_buffer(
         self, group: int, binding: int, buffer: GPUBuffer
     ) -> GPUBuffer:
-        self.bind_groups[group] = None
-        self.bindings[group][binding] = buffer
+        self.__bind_groups[group] = None
+        self.__bindings[group][binding] = buffer
         return buffer
 
     def set_binding_texture(
         self, group: int, binding: int, texture: GPUTexture
     ) -> GPUTexture:
-        self.bind_groups[group] = None
-        self.bindings[group][binding] = texture
+        self.__bind_groups[group] = None
+        self.__bindings[group][binding] = texture
         return texture
 
     def set_binding_sampler(
@@ -113,12 +113,12 @@ class AutoRenderPipeline:
     ) -> GPUSampler:
         if sampler is None:
             sampler = SamplerBuilder().build()
-        self.bind_groups[group] = None
-        self.bindings[group][binding] = sampler
+        self.__bind_groups[group] = None
+        self.__bindings[group][binding] = sampler
         return sampler
 
     def set_vertex_buffer(self, id: int, buffer: GPUBuffer) -> GPUBuffer:
-        self.vertex_buffers[id] = buffer
+        self.__vertex_buffers[id] = buffer
         return buffer
 
     def set_vertex_array(
@@ -138,8 +138,8 @@ class AutoRenderPipeline:
     def set_index_buffer(
         self, buffer: GPUBuffer, index_format: IndexFormat | str = IndexFormat.uint32
     ) -> GPUBuffer:
-        self.index_buffer = buffer
-        self.index_format = index_format
+        self.__index_buffer = buffer
+        self.__index_format = index_format
         return buffer
 
     def set_index_array(
@@ -161,9 +161,9 @@ class AutoRenderPipeline:
         return self.set_index_buffer(buffer, index_format)
 
     def set_depth_texture(self, texture: GPUTexture) -> GPUTexture:
-        if self.depth_texture is None:
-            self.pipeline = None  # must recreate the pipeline
-        self.depth_texture = texture
+        if self.__depth_texture is None:
+            self.__pipeline = None  # must recreate the pipeline
+        self.__depth_texture = texture
         return texture
 
     def create_depth_texture(self, size: tuple[int, int]) -> GPUTexture:
@@ -171,7 +171,7 @@ class AutoRenderPipeline:
         return self.set_depth_texture(texture)
 
     def with_depth_texture(self) -> Self:
-        self.with_depth = True
+        self.__with_depth = True
         return self
 
     def render(
@@ -181,65 +181,68 @@ class AutoRenderPipeline:
         instance_count: int = 1,
         clear: bool = True,
     ):
-        if self.with_depth and (
-            self.depth_texture is None
-            or self.depth_texture.width != output_texture.width
-            or self.depth_texture.height != output_texture.height
+        if self.__with_depth and (
+            self.__depth_texture is None
+            or self.__depth_texture.width != output_texture.width
+            or self.__depth_texture.height != output_texture.height
         ):
             self.create_depth_texture((output_texture.width, output_texture.height))
 
-        if self.output_format is None or self.output_format != output_texture.format:
-            self.output_format = output_texture.format
-            self.pipeline = None  # must recreate the pipeline
+        if (
+            self.__output_format is None
+            or self.__output_format != output_texture.format
+        ):
+            self.__output_format = output_texture.format
+            self.__pipeline = None  # must recreate the pipeline
 
-        if self.vertex_descriptors_builder is not None:
-            self.vertex_buffer_descriptors = self.vertex_descriptors_builder.build()
-            self.vertex_descriptors_builder = None
-            self.pipeline = None
+        if self.__vertex_descriptors_builder is not None:
+            self.__vertex_buffer_descriptors = self.__vertex_descriptors_builder.build()
+            self.__vertex_descriptors_builder = None
+            self.__pipeline = None
 
-        if self.pipeline is None:
+        if self.__pipeline is None:
             pipeline_builder = (
-                RenderPipelineBuilder(self.output_format)
-                .with_vertex_buffer_descriptors(self.vertex_buffer_descriptors)
-                .with_layout(self.reflection.get_pipeline_layout())
-                .with_shader_source(self.wgsl_source)
+                RenderPipelineBuilder(self.__output_format)
+                .with_vertex_buffer_descriptors(self.__vertex_buffer_descriptors)
+                .with_layout(self.__reflection.get_pipeline_layout())
+                .with_shader_source(self.__wgsl_source)
             )
 
-            if self.depth_texture is not None:
+            if self.__depth_texture is not None:
                 pipeline_builder.with_depth_stencil()
 
-            self.pipeline = pipeline_builder.build()
+            self.__pipeline = pipeline_builder.build()
 
-        for gid in self.bind_groups:
-            if self.bind_groups[gid] is None:
-                layout = self.reflection.get_bind_group_layout(gid)
+        for gid in self.__bind_groups:
+            if self.__bind_groups[gid] is None:
+                layout = self.__reflection.get_bind_group_layout(gid)
                 builder = BindGroupBuilder(layout)
-                for bid in self.bindings[gid]:
-                    if isinstance(self.bindings[gid][bid], GPUTexture):
-                        builder.with_texture(self.bindings[gid][bid], index=bid)
-                    elif isinstance(self.bindings[gid][bid], GPUSampler):
-                        builder.with_sampler(self.bindings[gid][bid], index=bid)
+                for bid in self.__bindings[gid]:
+                    if isinstance(self.__bindings[gid][bid], GPUTexture):
+                        builder.with_texture(self.__bindings[gid][bid], index=bid)
+                    elif isinstance(self.__bindings[gid][bid], GPUSampler):
+                        builder.with_sampler(self.__bindings[gid][bid], index=bid)
                     else:
-                        builder.with_buffer(self.bindings[gid][bid], index=bid)
-                self.bind_groups[gid] = builder.build()
+                        builder.with_buffer(self.__bindings[gid][bid], index=bid)
+                self.__bind_groups[gid] = builder.build()
         command_encoder = CommandBufferBuilder()
         render_pass_builder = command_encoder.begin_render_pass(
             output_texture
         ).with_load_op(LoadOp.clear if clear else LoadOp.load)
-        if self.depth_texture is not None:
-            render_pass_builder.with_depth_stencil(self.depth_texture, clear)
+        if self.__depth_texture is not None:
+            render_pass_builder.with_depth_stencil(self.__depth_texture, clear)
         render_pass = render_pass_builder.build()
-        render_pass.set_pipeline(self.pipeline)
-        for gid, g in self.bind_groups.items():
+        render_pass.set_pipeline(self.__pipeline)
+        for gid, g in self.__bind_groups.items():
             render_pass.set_bind_group(gid, g)
-        for id, buf in enumerate(self.vertex_buffers):
+        for id, buf in enumerate(self.__vertex_buffers):
             if buf is None:
                 raise ValueError(f"Vertex Buffer {id} not set")
             render_pass.set_vertex_buffer(id, buf)
-        if self.index_buffer is None:
+        if self.__index_buffer is None:
             render_pass.draw(vertex_count, instance_count)
         else:
-            render_pass.set_index_buffer(self.index_buffer, self.index_format)  # type: ignore
+            render_pass.set_index_buffer(self.__index_buffer, self.__index_format)  # type: ignore
             render_pass.draw_indexed(vertex_count, instance_count)
         render_pass.end()
 
