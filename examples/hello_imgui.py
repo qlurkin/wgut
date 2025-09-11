3eb67c8bc1a6b756ad25b2d34e8e2c54da2a1cdb
Quentin Lurkin <qlurkin@gmail.com>
2025-09-11 19:57:20 +0200

    Update Imgui Example

--- examples/hello_imgui.py
+++ examples/hello_imgui.py
@@ -2,30 +2,44 @@
 from imgui_bundle import imgui
 from wgpu.utils.imgui import ImguiRenderer
 
-from wgpu import BufferUsage, GPUTexture, IndexFormat, ShaderStage, VertexFormat
+from wgpu import (
+    BufferBindingType,
+    BufferUsage,
+    CompareFunction,
+    CullMode,
+    FrontFace,
+    GPURenderPassEncoder,
+    GPUTexture,
+    IndexFormat,
+    LoadOp,
+    PrimitiveTopology,
+    ShaderStage,
+    StoreOp,
+    TextureFormat,
+    TextureUsage,
+    VertexFormat,
+    VertexStepMode,
+)
 from wgut import (
-    BindGroupBuilder,
-    BingGroupLayoutBuilder,
-    RenderPipelineBuilder,
-    BufferBuilder,
-    CommandBufferBuilder,
-    PipelineLayoutBuilder,
-    TextureBuilder,
-    VertexBufferDescriptorsBuilder,
-    get_device,
     write_buffer,
     Window,
     load_file,
 )
 import numpy as np
 import wgut.cgmath as cm
+from wgut.core import get_device, submit_command
 
 
 class MyApp(Window):
     def on_resize(self):
         canvas = self.get_canvas()
-        self.depth_texture = TextureBuilder().build_depth(canvas.get_physical_size())
-        width, height = self.get_canvas().get_logical_size()
+        self.depth_texture = get_device().create_texture(
+            size=canvas.get_physical_size(),
+            format=TextureFormat.depth32float,  # type: ignore
+            usage=TextureUsage.RENDER_ATTACHMENT | TextureUsage.TEXTURE_BINDING,  # type: ignore
+        )
+
+        width, height = canvas.get_physical_size()
 
         view_matrix = cm.look_at([3, 2, 4], [0, 0, 0], [0, 1, 0])
         proj_matrix = cm.perspective(45, width / height, 0.1, 100)
@@ -88,46 +102,99 @@ class MyApp(Window):
         )
         # fmt: on
 
-        self.vertex_buffer = (
-            BufferBuilder()
-            .from_data(vertex_data)
-            .with_usage(BufferUsage.VERTEX)
-            .build()
+        self.vertex_buffer = get_device().create_buffer(
+            size=vertex_data.nbytes,
+            usage=BufferUsage.VERTEX | BufferUsage.COPY_DST,  # type: ignore
+        )
+        write_buffer(self.vertex_buffer, vertex_data)
+
+        self.index_buffer = get_device().create_buffer(
+            size=index_data.nbytes,
+            usage=BufferUsage.INDEX | BufferUsage.COPY_DST,  # type: ignore
+        )
+        write_buffer(self.index_buffer, index_data)
+
+        bg_layout = get_device().create_bind_group_layout(
+            entries=[
+                {
+                    "binding": 0,
+                    "visibility": ShaderStage.VERTEX,
+                    "buffer": {"type": BufferBindingType.uniform},
+                },
+            ]
         )
 
-        self.index_buffer = (
-            BufferBuilder().from_data(index_data).with_usage(BufferUsage.INDEX).build()
+        p_layout = get_device().create_pipeline_layout(bind_group_layouts=[bg_layout])
+
+        self.camera_buffer = get_device().create_buffer(
+            size=2 * 4 * 4 * 4,
+            usage=BufferUsage.UNIFORM | BufferUsage.COPY_DST,  # type: ignore
         )
 
-        bg_layout = BingGroupLayoutBuilder().with_buffer(ShaderStage.VERTEX).build()
-        p_layout = PipelineLayoutBuilder().with_bind_group_layout(bg_layout).build()
-
-        self.camera_buffer = (
-            BufferBuilder()
-            .with_size(2 * 4 * 4 * 4)
-            .with_usage(BufferUsage.UNIFORM | BufferUsage.COPY_DST)
-            .build()
+        self.camera_bind_group = get_device().create_bind_group(
+            layout=bg_layout,
+            entries=[
+                {
+                    "binding": 0,
+                    "resource": {
+                        "buffer": self.camera_buffer,
+                        "offset": 0,
+                        "size": self.camera_buffer.size,
+                    },
+                },
+            ],
         )
 
-        self.camera_bind_group = (
-            BindGroupBuilder(bg_layout).with_buffer(self.camera_buffer).build()
-        )
+        vertex_buffer_descriptor = {
+            "array_stride": 6 * 4,
+            "step_mode": VertexStepMode.vertex,
+            "attributes": [
+                {
+                    "format": VertexFormat.float32x3,
+                    "offset": 0,
+                    "shader_location": 0,
+                },
+                {
+                    "format": VertexFormat.float32x3,
+                    "offset": 3 * 4,
+                    "shader_location": 1,
+                },
+            ],
+        }
 
-        vertex_buffer_descriptors = (
-            VertexBufferDescriptorsBuilder()
-            .with_vertex_buffer()
-            .with_attribute(VertexFormat.float32x3)
-            .with_attribute(VertexFormat.float32x3)
-            .build()
-        )
+        shader_module = get_device().create_shader_module(code=load_file("./cube.wgsl"))
 
-        self.pipeline = (
-            RenderPipelineBuilder(self.get_texture_format())
-            .with_vertex_buffer_descriptors(vertex_buffer_descriptors)
-            .with_layout(p_layout)
-            .with_shader_source(load_file("cube.wgsl"))
-            .with_depth_stencil()
-            .build()
+        self.pipeline = get_device().create_render_pipeline(
+            layout=p_layout,
+            vertex={
+                "module": shader_module,
+                "entry_point": "vs_main",
+                "buffers": [vertex_buffer_descriptor],
+            },
+            primitive={
+                "topology": PrimitiveTopology.triangle_list,
+                "front_face": FrontFace.ccw,
+                "cull_mode": CullMode.back,
+            },
+            depth_stencil={
+                "format": TextureFormat.depth32float,
+                "depth_write_enabled": True,
+                "depth_compare": CompareFunction.less,
+            },
+            multisample=None,
+            fragment={
+                "module": shader_module,
+                "entry_point": "fs_main",
+                "targets": [
+                    {
+                        "format": self.get_texture_format(),
+                        "blend": {
+                            "color": {},
+                            "alpha": {},
+                        },
+                    },
+                ],
+            },
         )
 
         self.on_resize()
@@ -145,13 +212,26 @@ class MyApp(Window):
         self.frame_time = delta_time
 
     def render(self, screen: GPUTexture):
-        command_buffer_builder = CommandBufferBuilder()
+        command_encoder = get_device().create_command_encoder()
 
-        render_pass = (
-            command_buffer_builder.begin_render_pass(screen)
-            .with_depth_stencil(self.depth_texture)
-            .build()
+        render_pass: GPURenderPassEncoder = command_encoder.begin_render_pass(
+            color_attachments=[
+                {
+                    "view": screen.create_view(),
+                    "resolve_target": None,
+                    "clear_value": (0.9, 0.9, 0.9, 1.0),
+                    "load_op": LoadOp.clear,
+                    "store_op": StoreOp.store,
+                }
+            ],
+            depth_stencil_attachment={
+                "view": self.depth_texture.create_view(),
+                "depth_clear_value": 1.0,
+                "depth_load_op": LoadOp.clear,
+                "depth_store_op": StoreOp.store,
+            },
         )
+
         render_pass.set_pipeline(self.pipeline)
         render_pass.set_vertex_buffer(0, self.vertex_buffer)
         render_pass.set_index_buffer(self.index_buffer, IndexFormat.uint32)  # type: ignore
@@ -159,7 +239,7 @@ class MyApp(Window):
         render_pass.draw_indexed(36)
         render_pass.end()
 
-        command_buffer_builder.submit()
+        submit_command(command_encoder)
 
         self.imgui_renderer.render()
 
