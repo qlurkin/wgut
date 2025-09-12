09a920de1b9b82a6d3bb0c079e42a5e6024612f2
Quentin Lurkin <qlurkin@gmail.com>
2025-09-12 16:14:19 +0200

    Update Particles Example

--- examples/particles.py
+++ examples/particles.py
@@ -1,11 +1,17 @@
-from wgpu import BufferUsage, GPUBuffer
-from wgut.auto_compute_pipeline import AutoComputePipeline
-from wgut.builders.bufferbuilder import BufferBuilder
+from wgpu import (
+    BufferBindingType,
+    BufferUsage,
+    GPUBindGroupLayout,
+    GPUCommandEncoder,
+    GPUComputePassEncoder,
+    GPUComputePipeline,
+    ShaderStage,
+)
+
 from wgut.cgmath import vec4
-from wgut.core import load_file, write_buffer
+from wgut.core import get_device, load_file, read_buffer, submit_command, write_buffer
 from wgut.orbit_camera import OrbitCamera
 from wgut.scene.instance_mesh import InstanceMesh
-from wgut.scene.particles import Particles
 from wgut.scene.performance_monitor import performance_monitor
 from wgut.scene.render_gui_system import render_gui_system
 from wgut.scene.render_system import (
@@ -16,22 +22,62 @@ from wgut.scene.render_system import (
     MeshComponent,
     render_system,
 )
-from wgut.scene.renderer import Renderer
+from wgut.scene.renderer import Renderer, Material
 from wgut.scene.window_system import window_system
 from wgut.scene.ecs import ECS
-from wgut.scene.basic_color_material import BasicColorMaterial
 from wgut.scene.primitives.icosphere import icosphere
 from wgut.scene.transform import Transform
+from wgut.scene.ambiant_light import AmbiantLight
 import random
 import numpy as np
 
 default_layer = Layer("default")
 
 
+def create_compute_pipeline() -> tuple[GPUComputePipeline, GPUBindGroupLayout]:
+    shader_source = load_file("./particles.wgsl")
+
+    shader_module = get_device().create_shader_module(code=shader_source)
+
+    bind_group_layout = get_device().create_bind_group_layout(
+        entries=[
+            {
+                "binding": 0,
+                "visibility": ShaderStage.COMPUTE,
+                "buffer": {"type": BufferBindingType.storage},
+            },
+            {
+                "binding": 1,
+                "visibility": ShaderStage.COMPUTE,
+                "buffer": {"type": BufferBindingType.read_only_storage},
+            },
+            {
+                "binding": 2,
+                "visibility": ShaderStage.COMPUTE,
+                "buffer": {"type": BufferBindingType.uniform},
+            },
+        ]
+    )
+
+    pipeline_layout = get_device().create_pipeline_layout(
+        bind_group_layouts=[bind_group_layout]
+    )
+
+    pipeline = get_device().create_compute_pipeline(
+        layout=pipeline_layout,
+        compute={
+            "module": shader_module,
+            "entry_point": "main",
+        },
+    )
+
+    return pipeline, bind_group_layout
+
+
 def setup(ecs: ECS):
     mesh = icosphere(2)
 
-    computer = AutoComputePipeline(load_file("particles.wgsl"))
+    pipeline, bind_group_layout = create_compute_pipeline()
 
     positions = []
     for _ in range(128):
@@ -49,37 +95,79 @@ def setup(ecs: ECS):
             )
         )
 
-    velocities_buffer = (
-        BufferBuilder()
-        .from_data(np.array(velocities))
-        .with_usage(BufferUsage.STORAGE)
-        .build()
+    positions_array = np.array(positions)
+    positions_buffer = get_device().create_buffer(
+        size=positions_array.nbytes,
+        usage=BufferUsage.STORAGE | BufferUsage.COPY_DST | BufferUsage.COPY_SRC,  # type: ignore
+    )
+    write_buffer(positions_buffer, positions_array)
+
+    velocities_array = np.array(velocities)
+    velocities_buffer = get_device().create_buffer(
+        size=velocities_array.nbytes,
+        usage=BufferUsage.STORAGE | BufferUsage.COPY_DST,  # type: ignore
+    )
+    write_buffer(velocities_buffer, velocities_array)
+
+    dt_buffer = get_device().create_buffer(
+        size=4,
+        usage=BufferUsage.UNIFORM | BufferUsage.COPY_DST,  # type: ignore
     )
 
-    dt_buffer = (
-        BufferBuilder()
-        .with_size(4)
-        .with_usage(BufferUsage.UNIFORM | BufferUsage.COPY_DST)
-        .build()
+    bind_group = get_device().create_bind_group(
+        layout=bind_group_layout,
+        entries=[
+            {
+                "binding": 0,
+                "resource": {
+                    "buffer": positions_buffer,
+                    "offset": 0,
+                    "size": positions_buffer.size,
+                },
+            },
+            {
+                "binding": 1,
+                "resource": {
+                    "buffer": velocities_buffer,
+                    "offset": 0,
+                    "size": velocities_buffer.size,
+                },
+            },
+            {
+                "binding": 2,
+                "resource": {
+                    "buffer": dt_buffer,
+                    "offset": 0,
+                    "size": dt_buffer.size,
+                },
+            },
+        ],
     )
 
-    def callback(ecs: ECS, buffer: GPUBuffer, delta_time: float):
+    def update(ecs: ECS, delta_time: float):
         write_buffer(dt_buffer, np.array(delta_time, dtype=np.float32))
-        computer.set_buffer(0, 0, buffer)
-        computer.set_buffer(0, 1, velocities_buffer)
-        computer.set_buffer(0, 2, dt_buffer)
-        computer.dispatch(1)
 
-    particles = Particles(
-        np.array(positions),
-        callback,
-    )
+        command_encoder: GPUCommandEncoder = get_device().create_command_encoder()
+        compute_pass: GPUComputePassEncoder = command_encoder.begin_compute_pass()
 
-    particle_mesh = InstanceMesh(mesh, particles.get_translations)
+        compute_pass.set_pipeline(pipeline)
+        compute_pass.set_bind_group(index=0, bind_group=bind_group)
+        compute_pass.dispatch_workgroups(1)
+        compute_pass.end()
 
-    ecs.on("update", particles.update)
+        submit_command(command_encoder)
 
-    material = BasicColorMaterial((1.0, 0.0, 0.0))
+    def get_translations():
+        mem = read_buffer(positions_buffer)
+        translations = np.frombuffer(mem.cast("f"), dtype=np.float32)
+        translations = translations.reshape((translations.size // 4, 4))
+        return translations
+
+    particle_mesh = InstanceMesh(mesh, get_translations)
+
+    ecs.on("update", update)
+
+    material = Material((1.0, 0.0, 0.0, 1.0))
 
     ecs.spawn(
         [
@@ -93,6 +181,7 @@ def setup(ecs: ECS):
     camera = OrbitCamera((6, 4, 5), (0, 0, 0), 45, 0.1, 1000)
 
     ecs.spawn([CameraComponent(camera), ActiveCamera()], label="Camera")
+    ecs.spawn(AmbiantLight.create(color=(1.0, 1.0, 1.0)))
 
 
 def process_event(ecs: ECS, event):
@@ -101,7 +190,7 @@ def process_event(ecs: ECS, event):
         cam.camera.process_event(event)
 
 
-renderer = Renderer(30000, 150000, 512, 2, (1024, 1024, 32), 256)
+renderer = Renderer(30000, 150000, 2, 1024, 32)
 
 
 (
