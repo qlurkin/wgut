4c5b0c3474775575c21d6339b39ba88150b50663
Quentin Lurkin <qlurkin@gmail.com>
2025-09-12 12:35:13 +0200

    Update shadertoy

--- src/wgut/shadertoy.py
+++ src/wgut/shadertoy.py
@@ -1,15 +1,9 @@
 from imgui_bundle import imgui
+from numpy.typing import NDArray
 from wgpu.utils.imgui import ImguiRenderer
 
-from wgut.builders import (
-    BindGroupBuilder,
-    BingGroupLayoutBuilder,
-    BufferBuilder,
-    CommandBufferBuilder,
-    PipelineLayoutBuilder,
-    RenderPipelineBuilder,
-)
 from wgut.core import (
+    submit_command,
     write_buffer,
     get_device,
 )
@@ -111,6 +105,14 @@ class ShaderToy(Window):
         resolution = self.get_canvas().get_physical_size()
         return np.array([resolution[0], resolution[1], 0.0], dtype=np.float32)
 
+    def create_buffer(self, data: NDArray):
+        buffer = get_device().create_buffer(
+            size=data.nbytes,
+            usage=wgpu.BufferUsage.COPY_DST | wgpu.BufferUsage.UNIFORM,  # type: ignore
+        )
+        write_buffer(buffer, data)
+        return buffer
+
     def setup(self):
         self.set_title("ShaderToy")
         self.time = 0.0
@@ -119,64 +121,119 @@ class ShaderToy(Window):
         self.is_mouse_down = False
         self.is_mouse_up_in_this_frame = False
 
-        self.iResolutionBuffer = (
-            BufferBuilder()
-            .from_data(self.getIResolution())
-            .with_usage(wgpu.BufferUsage.COPY_DST | wgpu.BufferUsage.UNIFORM)
-            .build()
-        )
-        self.iTimeBuffer = (
-            BufferBuilder()
-            .from_data(self.getITime())
-            .with_usage(wgpu.BufferUsage.COPY_DST | wgpu.BufferUsage.UNIFORM)
-            .build()
-        )
-        self.iTimeDeltaBuffer = (
-            BufferBuilder()
-            .from_data(self.getITimeDelta(0.0))
-            .with_usage(wgpu.BufferUsage.COPY_DST | wgpu.BufferUsage.UNIFORM)
-            .build()
-        )
-        self.iDateBuffer = (
-            BufferBuilder()
-            .from_data(self.getIDate())
-            .with_usage(wgpu.BufferUsage.COPY_DST | wgpu.BufferUsage.UNIFORM)
-            .build()
-        )
-        self.iMouseBuffer = (
-            BufferBuilder()
-            .from_data(self.getIMouse())
-            .with_usage(wgpu.BufferUsage.COPY_DST | wgpu.BufferUsage.UNIFORM)
-            .build()
+        self.iResolutionBuffer = self.create_buffer(self.getIResolution())
+        self.iTimeBuffer = self.create_buffer(self.getITime())
+        self.iTimeDeltaBuffer = self.create_buffer(self.getITimeDelta(0.0))
+        self.iDateBuffer = self.create_buffer(self.getIDate())
+        self.iMouseBuffer = self.create_buffer(self.getIMouse())
+
+        bg_layout = get_device().create_bind_group_layout(
+            entries=[
+                {
+                    "binding": 0,
+                    "visibility": wgpu.ShaderStage.VERTEX | wgpu.ShaderStage.FRAGMENT,
+                    "buffer": {"type": wgpu.BufferBindingType.uniform},
+                },
+                {
+                    "binding": 1,
+                    "visibility": wgpu.ShaderStage.VERTEX | wgpu.ShaderStage.FRAGMENT,
+                    "buffer": {"type": wgpu.BufferBindingType.uniform},
+                },
+                {
+                    "binding": 2,
+                    "visibility": wgpu.ShaderStage.VERTEX | wgpu.ShaderStage.FRAGMENT,
+                    "buffer": {"type": wgpu.BufferBindingType.uniform},
+                },
+                {
+                    "binding": 3,
+                    "visibility": wgpu.ShaderStage.VERTEX | wgpu.ShaderStage.FRAGMENT,
+                    "buffer": {"type": wgpu.BufferBindingType.uniform},
+                },
+                {
+                    "binding": 4,
+                    "visibility": wgpu.ShaderStage.VERTEX | wgpu.ShaderStage.FRAGMENT,
+                    "buffer": {"type": wgpu.BufferBindingType.uniform},
+                },
+            ]
         )
 
-        bg_layout = (
-            BingGroupLayoutBuilder()
-            .with_buffer(wgpu.ShaderStage.VERTEX | wgpu.ShaderStage.FRAGMENT)
-            .with_buffer(wgpu.ShaderStage.VERTEX | wgpu.ShaderStage.FRAGMENT)
-            .with_buffer(wgpu.ShaderStage.VERTEX | wgpu.ShaderStage.FRAGMENT)
-            .with_buffer(wgpu.ShaderStage.VERTEX | wgpu.ShaderStage.FRAGMENT)
-            .with_buffer(wgpu.ShaderStage.VERTEX | wgpu.ShaderStage.FRAGMENT)
-            .build()
+        p_layout = get_device().create_pipeline_layout(bind_group_layouts=[bg_layout])
+
+        shader_module = get_device().create_shader_module(code=self.shader)
+
+        self.pipeline = get_device().create_render_pipeline(
+            layout=p_layout,
+            vertex={
+                "module": shader_module,
+                "entry_point": "vs_main",
+                "buffers": [],
+            },
+            primitive={
+                "topology": wgpu.PrimitiveTopology.triangle_list,
+                "front_face": wgpu.FrontFace.ccw,
+                "cull_mode": wgpu.CullMode.back,
+            },
+            depth_stencil=None,
+            multisample=None,
+            fragment={
+                "module": shader_module,
+                "entry_point": "fs_main",
+                "targets": [
+                    {
+                        "format": self.get_texture_format(),
+                        "blend": {
+                            "color": {},
+                            "alpha": {},
+                        },
+                    },
+                ],
+            },
         )
 
-        p_layout = PipelineLayoutBuilder().with_bind_group_layout(bg_layout).build()
-
-        self.pipeline = (
-            RenderPipelineBuilder(self.get_texture_format())
-            .with_layout(p_layout)
-            .with_shader_source(self.shader)
-            .build()
-        )
-
-        self.bind_group = (
-            BindGroupBuilder(bg_layout)
-            .with_buffer(self.iResolutionBuffer)
-            .with_buffer(self.iTimeBuffer)
-            .with_buffer(self.iTimeDeltaBuffer)
-            .with_buffer(self.iDateBuffer)
-            .with_buffer(self.iMouseBuffer)
-            .build()
+        self.bind_group = get_device().create_bind_group(
+            layout=bg_layout,
+            entries=[
+                {
+                    "binding": 0,
+                    "resource": {
+                        "buffer": self.iResolutionBuffer,
+                        "offset": 0,
+                        "size": self.iResolutionBuffer.size,
+                    },
+                },
+                {
+                    "binding": 1,
+                    "resource": {
+                        "buffer": self.iTimeBuffer,
+                        "offset": 0,
+                        "size": self.iTimeBuffer.size,
+                    },
+                },
+                {
+                    "binding": 2,
+                    "resource": {
+                        "buffer": self.iTimeDeltaBuffer,
+                        "offset": 0,
+                        "size": self.iTimeDeltaBuffer.size,
+                    },
+                },
+                {
+                    "binding": 3,
+                    "resource": {
+                        "buffer": self.iDateBuffer,
+                        "offset": 0,
+                        "size": self.iDateBuffer.size,
+                    },
+                },
+                {
+                    "binding": 4,
+                    "resource": {
+                        "buffer": self.iMouseBuffer,
+                        "offset": 0,
+                        "size": self.iMouseBuffer.size,
+                    },
+                },
+            ],
         )
 
         self.imgui_renderer = ImguiRenderer(
@@ -210,16 +267,28 @@ class ShaderToy(Window):
         write_buffer(self.iMouseBuffer, self.getIMouse())
 
     def render(self, screen: wgpu.GPUTexture):
-        command_encoder = CommandBufferBuilder()
+        # command_encoder = CommandBufferBuilder()
+        command_encoder = get_device().create_command_encoder()
 
-        render_pass = command_encoder.begin_render_pass(screen).build()
+        # render_pass = command_encoder.begin_render_pass(screen).build()
+        render_pass: wgpu.GPURenderPassEncoder = command_encoder.begin_render_pass(
+            color_attachments=[
+                {
+                    "view": screen.create_view(),
+                    "resolve_target": None,
+                    "clear_value": (0.0, 0.0, 0.0, 1.0),
+                    "load_op": wgpu.LoadOp.clear,
+                    "store_op": wgpu.StoreOp.store,
+                }
+            ],
+        )
 
         render_pass.set_pipeline(self.pipeline)
         render_pass.set_bind_group(0, self.bind_group)
         render_pass.draw(6)
         render_pass.end()
 
-        command_encoder.submit()
+        submit_command(command_encoder)
 
         self.imgui_renderer.render()
 
