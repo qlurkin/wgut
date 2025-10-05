e75fcb666ba5cab80b7bec086e8b3d7381f63867
Quentin Lurkin <qlurkin@gmail.com>
2025-10-05 16:09:07 +0200

    Fix import paths

--- src/wgut/__init__.py
+++ src/wgut/__init__.py
@@ -1,12 +1,55 @@
-# ruff: noqa: F401, F403
-# type: ignore
-
 from wgut.shadertoy import ShaderToy
-from wgut.core import *
+from wgut.core import (
+    get_adapter,
+    get_shared,
+    get_device,
+    print_adapter_info,
+    read_buffer,
+    read_pygfx_buffer,
+    write_buffer,
+    write_pygfx_buffer,
+    write_texture,
+    load_image,
+    create_texture,
+    create_canvas,
+    load_file,
+    submit_command,
+)
 from wgut.window import Window
-from wgut.render_system import *
+from wgut.render_system import (
+    SceneObject,
+    render_system,
+    ActiveCamera,
+)
 from wgut.render_gui_system import render_gui_system
 from wgut.window_system import window_system
 from wgut.ecs import ECS
 from wgut.performance_monitor import performance_monitor
 from wgut.ecs_explorer import ecs_explorer
+
+__all__ = [
+    "ShaderToy",
+    "Window",
+    "render_gui_system",
+    "window_system",
+    "ECS",
+    "performance_monitor",
+    "ecs_explorer",
+    "get_adapter",
+    "get_shared",
+    "get_device",
+    "print_adapter_info",
+    "read_buffer",
+    "read_pygfx_buffer",
+    "write_buffer",
+    "write_pygfx_buffer",
+    "write_texture",
+    "load_image",
+    "create_texture",
+    "create_canvas",
+    "load_file",
+    "submit_command",
+    "SceneObject",
+    "render_system",
+    "ActiveCamera",
+]
